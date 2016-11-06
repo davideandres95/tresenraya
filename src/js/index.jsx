ca5729d658ef3d347995cf74d4cc4ba24114e8fc
davideandres95 <davideandres95@gmail.com>
2016-11-06 09:23:00 +0100

    initial

---
+++ src/js/index.jsx
@@ -0,0 +1,10 @@
+var App = React.createClass({
+	render: function () {
+ 		return (
+ 			<div>
+ 				{this.props.valor}
+ 			</div>
+ 		);
+	 }
+});
+ReactDOM.render(<App valor="X" />, document.getElementById('contenedor'));
